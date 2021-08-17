189fac7b59565de35f12fa55b5fedc12bbb75778
vitor-bitencourt <86494335+vitor-bitencourt@users.noreply.github.com>
2021-08-17 11:59:32 -0300

    Delete functions directory

--- functions/conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conexao = mysqli_connect("localhost","root","","farme") or die ("Erro de conexão com localhost, o seguinte erro ocorreu --> ".mysqli_connect_error());
-include 'functions.php';
-$pdo = pdo_connect_mysql();
-?>
